f0d00a7c2242b6fa7c86e04e8124a161c5f6eb0f
Giangittop1 <giang10a1dz@gmail.com>
2022-08-31 04:41:42 +0700

    commit

--- src/component/Home/Banner/Banner.jsx
+++ src/component/Home/Banner/Banner.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import "./Banner.sass"
+import CampaignIcon from '@mui/icons-material/Campaign';
 
 const Banner = (props) => {
   return (
     <div className="banner-home">
-      
+      <SubBanner /> 
+      <Discount {...props} />
+      <Support {...props} />
     </div>
   )
+}
+
+const SubBanner = (props) => {
+    return (
+        <div className="sub-banner">
+            <img className="sub-banner-icon" alt='open' src={"https://res.cloudinary.com/dxkhlwfz3/image/upload/v1661895564/facebook-advertising.9544591b_pshumi.png"} />
+        </div>
+    )
+
+}
+
+const Discount= (props) => {
+    const array_discount= [{text: "Từ 0 đến 100k +30% giá trị thẻ nạp"}, {text: "Từ 500k + 50% giá trị thẻ nạp"}, {text: "Từ 2000k + 100% giá trị thẻ nạp"}, {text: "Tiền khuyến mãi chỉ mua được Mail (Hotmail + Outlook + Domain)"}]
+    return (
+        <div className="banner-discount">
+            <div className="banner-discount-title">Khuyến mãi nạp</div>
+            {
+                array_discount.map((item, key)=> <div key={key} className="banner-item-content"><CampaignIcon /> {item.text}</div>)    
+            }
+        </div>
+    )
+}
+
+const Support= (props)=> {
+    const array_support= [{text: "Group Zalo nhận thông báo quan trọng"}, {text: "Video HD sử dụng Tool Check Live"}, {text: "Mọi thắc mắc vui lòng liên hệ Zalo"}]
+    return (
+        <div className="banner-support">
+            <div className="banner-support-title">Hỗ trợ</div>
+            {
+                array_support.map((item, key)=> <div key={key} className="banner-item-content"><CampaignIcon /> {item.text}</div>)    
+            }
+        </div>
+    )
 }
 
 export default Banner
