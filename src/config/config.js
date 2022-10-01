d4d179b34fd466f4ce4e6240344ad5d70f19285f
datispham <datistpham@gmail.com>
2022-10-01 15:40:16 +0700

    upload

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "http://localhost:4000";
+export const SERVER_URL = "https://trick-final.herokuapp.com";
 // https://tricklor-server.herokuapp.com
 // https://trick-final.herokuapp.com
